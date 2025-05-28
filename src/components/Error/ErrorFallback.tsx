f97ddf47a51304e078363028289610a373be16ff
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 15:16:21 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/components/Error/ErrorFallback.tsx
+++ src/components/Error/ErrorFallback.tsx
@@ -3,14 +3,9 @@ import { Stack } from "@deskpro/deskpro-ui";
 import { DEFAULT_ERROR } from "../../constants";
 import { SageError } from "../../services/sage";
 import { Container, ErrorBlock } from "../common";
-import type { FC } from "react";
-import type { FallbackProps } from "react-error-boundary";
+import { FallbackRender } from "@sentry/react";
 
-type Props = Omit<FallbackProps, "error"> & {
-  error: Error,
-};
-
-const ErrorFallback: FC<Props> = ({ error }) => {
+const ErrorFallback: FallbackRender = ({ error }) => {
   let message = DEFAULT_ERROR;
 
   // eslint-disable-next-line no-console
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/react';
 import './instrument';
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
@@ -15,9 +14,10 @@ import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "./main.css";
 import "simplebar/dist/simplebar.min.css";
 import { Scrollbar } from "@deskpro/deskpro-ui";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
-  onRecoverableError: Sentry.reactErrorHandler(),
+  onRecoverableError: reactErrorHandler(),
 });
 root.render((
   <React.StrictMode>
@@ -26,9 +26,9 @@ root.render((
         <HashRouter>
           <QueryClientProvider client={queryClient}>
             <Suspense fallback={<LoadingSpinner />}>
-              <Sentry.ErrorBoundary FallbackComponent={ErrorFallback}>
+              <ErrorBoundary fallback={ErrorFallback}>
                 <App />
-              </Sentry.ErrorBoundary>
+              </ErrorBoundary>
             </Suspense>
           </QueryClientProvider>
         </HashRouter>
